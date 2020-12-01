fe2bf6e74b2b4536e1afd3fab3f78b5c735322b1
Alexander <i@numart.ru>
2020-12-01 17:42:30 +0300

    task 16.1

--- cracking/16_1.py
+++ cracking/16_1.py
@@ -1,3 +1,19 @@
 # 16.1 Number Swapper: Write a function to swap a number in place (that is,
 # without temporary variables).
 
+from random import randint
+
+
+def swap(x, y):
+    x ^= y
+    y ^= x
+    x ^= y
+    return x, y
+
+
+if __name__ == "__main__":
+    for _ in range(1000):
+        x, y = randint(-int(1e9), int(1e9)), randint(-int(1e9), int(1e9))
+        xs, ys = swap(x, y)
+        assert x == ys and y == xs
+    print("yay!")
