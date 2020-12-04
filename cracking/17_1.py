2dc03f1c85802d69ecd83e9a307a3133db9c7021
Alexander <i@numart.ru>
2020-12-04 19:32:00 +0300

    task 17.1 and 2

--- cracking/17_1.py
+++ cracking/17_1.py
@@ -1,2 +1,22 @@
 # 17.1 Add Without Plus: Write a function that adds two numbers. You should not
 # use + or any arithmetic operators.
+
+from random import randrange
+
+
+def add(a, b):
+    while b != 0:
+        # XOR is a bit sum without carrying to higher bits
+        s = a ^ b
+        # if both bits are 1, we need to carry 1 to the next bit
+        carry = (a & b) << 1
+        # iterate until there's nothing to carry
+        a, b = s, carry
+    return a
+
+
+if __name__ == "__main__":
+    for _ in range(100):
+        a, b = randrange(10 ** 9), randrange(10 ** 9)
+        assert add(a, b) == a + b
+    print("yay")
--- cracking/17_2.py
+++ cracking/17_2.py
@@ -2,3 +2,24 @@
 # shuffle - in other words, each of 52! permutations of the deck has to be
 # equally likely. Assume that you are givan a random number generator which
 # is perfect.
+
+from random import randrange
+
+
+def shuffle(arr):
+    # if len(arr) == 2, we simply swap once with p=1/2
+    # if len(arr) == n, we shuffle arr[:n-1] and
+    # we swap n-th element with any previous one with p=1/n
+    for i in range(1, len(arr)):
+        k = randrange(i + 1)
+        arr[i], arr[k] = arr[k], arr[i]
+    return arr
+
+
+if __name__ == "__main__":
+    a = [0] * 5
+    for _ in range(10 ** 5):
+        s = shuffle(list(range(len(a))))
+        for i, v in enumerate(s):
+            a[i] += v
+    print(a)
